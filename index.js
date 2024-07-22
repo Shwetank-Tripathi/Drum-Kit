2efbb4b6618058f13de514a675d676bd79b13b5a
Shwetank Tripathi <swetankvns@gmail.com>
2024-07-23 00:26:47 +0530

    edited

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ for(var i=0;i<numberOfDrumButtons;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function(){
         var buttonInnerHTML = this.innerHTML;
         makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML)
+        activeButton.classList.remove("pressed");
     });
 
 }
 
 document.addEventListener("keydown",function(event){
     makeSound(event.key);
-    console.log(event);
+    buttonAnimation(event.key);    
 
 });
 
@@ -48,5 +50,15 @@ function makeSound(key){
             kickbass.play();
         break;
     }
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
+
 }
 
